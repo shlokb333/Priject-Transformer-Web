277ac3335afc6adf6f1cd7f596c9a3a672a55d79
Pranit2411 <pranitmenkar24@gmail.com>
2023-03-22 17:06:05 +0530

    New Commit with File Changes

---
+++ JS_Input_script.js
@@ -0,0 +1,17 @@
+const prev = document.getElementById('previous-btn');
+adminBtn.addEventListener('click', () => {
+    window.location.href = "choose-type.html";
+    // Add code to redirect to admin.html page
+});
+
+const next = document.getElementById('current-btn');
+adminBtn.addEventListener('click', () => {
+    window.location.href = "input-ct.html";
+    // Add code to redirect to admin.html page
+});
+
+const compute = document.getElementById('current-btn');
+adminBtn.addEventListener('click', () => {
+    window.location.href = "input-ct.html";
+    // Add code to redirect to admin.html page
+});
